b3bd6060081f2b790eac11eb3888642ae4c0e4ad
pareshpbhayani <pareshpbhayani@gmail.com>
2024-08-23 19:35:13 +0530

    Add project files.

---
+++ XMLDocumentationDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyNamespace;
+
+var myMaths = new MyMaths();
+
+Console.WriteLine(myMaths.Subtract(1, 2).ToString());
+
+namespace MyNamespace
+{
+    public class MyMaths
+    {
+
+        /// <summary>
+        /// Subtracts one number from another.
+        /// </summary>
+        /// <param name="a">The number to subtract from.</param>
+        /// <param name="b">The number to subtract.</param>
+        /// <returns>The difference between the two numbers.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when b is greater than a.</exception>
+        public int Subtract(int a, int b)
+        {
+            if (b > a)
+                throw new ArgumentOutOfRangeException("b", "b cannot be greater than a.");
+
+            return a - b;
+        }
+    }
+}
+
